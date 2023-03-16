78f294f6a6081fe6e849af69e76c71dcc70345d7
The24Reem <127919699+The24Reem@users.noreply.github.com>
2023-03-16 14:03:04 +0530

    Create arravg.c

---
+++ arravg.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+   int arr[5];
+   int p=0;
+   for(int i=0;i<5;i++)
+   {
+       scanf("%d",&arr[i]);
+   }
+   for(int j=0;j<5;j++)
+   {
+       p=p+arr[j];
+   }
+   printf("%d",p/5);
+
+    return 0;
+}
